1ebf32576bfec7c3e5ab62213b6ffa40dff2edbf
junior <junior.oliveira05@icloud.com>
2024-08-02 10:51:04 -0300

    add Biblioteca phosphor

--- desafio-01/src/components/Header.tsx
+++ desafio-01/src/components/Header.tsx
@@ -1,11 +1,10 @@
-
+import { Rocket } from "phosphor-react";
 import styles from './Header.module.css'
-import img from '../assets/rocket.png'
 
 export function Header () {
     return (
         <header className={styles.header}>
-                <img src={img} alt="LOGO" />
+                <Rocket size={56} />
                 <strong>to<span>do</span></strong>
         </header>
     )
